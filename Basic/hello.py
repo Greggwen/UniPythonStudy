34f74d319c6801d6d5c57b355b2386c824e4c956
Greggwen <simple.xull@gmail.com>
2019-08-12 18:50:27 +0800

    first python file

---
+++ Basic/hello.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import platform
+
+"""
+在这里需要注意的是： python3.x 源码文件默认使用utf-8编码，所以可以正常解析中文。
+而python 2.x默认编码格式是 ASCII 格式，所以如果使用中文需要在开头添加： # -*- coding: UTF-8 -*-  或者  # coding=utf-8
+# coding=utf-8
+"""
+
+print("Hello Python!")
+print("你好，Python3")
+
+
+print("使用Python当前版本为： " + platform.python_version())
