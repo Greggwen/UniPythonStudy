c2c5fa1033504766e89ab37849f976cdaaaad5d4
Greggwen <simple.xull@gmail.com>
2020-03-16 19:07:40 +0800

    hash exec

--- 09-Redis/02-HashTable/hash_base.py
+++ 09-Redis/02-HashTable/hash_base.py
@@ -27,3 +27,46 @@ print(redis_client.hgetall('month'))
 
 
 # HSETNX hash field value， 当field 不存在hash表时，将它设置为value
+
+redis_client.hsetnx('month', 'Jan', 'Jan.')
+redis_client.hsetnx('month', 'July', 'July')
+
+print(redis_client.hget('month', 'Jan'))  # output "January"
+print(redis_client.hget('month', 'July')) # output "July"
+
+
+# HEXISTS hash field ， 检测field是否在hash中
+print(redis_client.hexists('month', 'Jan'))
+print(redis_client.hexists('month', 'Oct'))
+
+# HDEL hash field ，删除hash中的field，支持同时删除多个，并返回删除成功的个数，不存在的field会被忽略
+
+ret = redis_client.hdel('month', 'Jan', 'July', 'Mar')
+
+print('--------------------------------')
+print(ret)
+print(redis_client.hget('month', 'Jan'))
+print(redis_client.hget('month', 'July'))
+print(redis_client.hget('month', 'Mar'))
+
+# HLEN key 获取field 个数
+print(redis_client.hlen('month'))
+
+# HSTRLEN hash field，返回hash中给定field的值的长度
+print(redis_client.hstrlen('month', 'May'))
+
+# HINCRBY hash field increment_value
+redis_client.hincrby('counter', 'page_view', 10)
+
+print(redis_client.hget('counter', 'page_view'))
+
+redis_client.hincrby('counter', 'page_view', 10)
+
+print(redis_client.hget('counter', 'page_view'))
+
+# HINCRBYFLOAT hash field incremtn_value
+redis_client.hincrbyfloat('milk', 'price', 10.0)
+print(redis_client.hget('milk', 'price'))
+redis_client.hincrbyfloat('milk', 'price', 0.2)
+print(redis_client.hget('milk', 'price'))
+
---
+++ 09-Redis/03-List/list_base.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379)
